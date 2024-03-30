df879a070d290e453607f472185896f57b6de75f
Samane Khademi <s.khademi1992@gmail.com>
2024-03-31 01:37:13 +0330

    Template out the website

--- store/views.py
+++ store/views.py
@@ -5,3 +5,7 @@ from .models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
+
+
+def about(request):
+    return render(request, 'about.html', {})
