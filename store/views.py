e7d64de66720d97252c325f73863b9465db76f02
Samane Khademi <s.khademi1992@gmail.com>
2024-04-03 03:14:07 +0330

    Login and logout

--- store/views.py
+++ store/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def index(request):
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
 
-
 def about(request):
     return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'با موفقیت وارد شدید.')
+            return redirect('home')
+        else:
+            messages.error(request, 'مشکلی در ورود به صفحه کاربری وجود دارد! لطفاِ دوباره امتحان کنید.')
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'با موفقیت خارج شدید.')
+    return redirect('home')
