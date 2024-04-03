66f1add7041a592e4924ec1e527f90d4f8d81125
Samane Khademi <s.khademi1992@gmail.com>
2024-04-03 04:42:51 +0330

    Register new users

--- store/views.py
+++ store/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
+
 
 def index(request):
     products = Product.objects.all()
@@ -29,3 +34,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'با موفقیت خارج شدید.')
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # Log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'ثبت نام با موفقیت انجام شد. به صفحه کاربری وارد شدید.')
+            return redirect('home')
+        else:
+            messages.success(request, 'مشکلی در ثبت نام وجود دارد! لطفا دوباره امتحان کنید.')
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form})
