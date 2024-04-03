48eda46be9282bdec7d8200b454f26b52807e6e5
Samane Khademi <s.khademi1992@gmail.com>
2024-04-03 18:14:45 +0330

    Product pages

--- store/views.py
+++ store/views.py
@@ -7,6 +7,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .forms import SignUpForm
 
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'product.html', {'product': product})
 
 def index(request):
     products = Product.objects.all()
