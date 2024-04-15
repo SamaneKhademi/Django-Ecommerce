9cc25d6c727273321ccebe4ef6e1c8b350203d9c
Samane Khademi <s.khademi1992@gmail.com>
2024-04-15 21:51:57 +0330

    Order checkout - fix shipping model problem

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product, Order
+from django.db.models.signals import post_save
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -19,6 +20,15 @@ class ShippingAddress(models.Model):
 
     def __str__(self):
         return f'Shipping Address - {str(self.id)}'
+
+#create a user shipping address by default when user sign up
+def create_shipping(sender, instance, created, **kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+# Automate the profile thing
+post_save.connect(create_shipping, sender=User)
 
 # Create order model
 class Order(models.Model):
--- payment/urls.py
+++ payment/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('payment_success/', views.payment_success, name='payment_success'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
