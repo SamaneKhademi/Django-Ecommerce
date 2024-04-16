1f196307640c48f6567477bfc74513fe5db44e38
Samane Khademi <s.khademi1992@gmail.com>
2024-04-16 10:08:01 +0330

    Billing info

--- payment/forms.py
+++ payment/forms.py
@@ -24,3 +24,26 @@ class ShippingForm(forms.ModelForm):
         fields = ['shipping_full_name', 'shipping_email', 'shipping_address1', 'shipping_address2', 'shipping_city', 'shipping_state', 'shipping_zipcode', 'shipping_country']
 
         exclude = ['user']
+
+
+class PaymentForm(forms.Form):
+    card_name = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'اسم'}),
+                            required=True)
+    card_number = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'شماره کارت'}),
+                            required=True)
+    card_exp_date = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'تاریخ انقضا'}),
+                            required=True)
+    card_cvv2_number = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'cvv2'}),
+                            required=True)
+    card_address1 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'آدرس 1'}),
+                            required=True)
+    card_address2 = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'آدرس 2'}),
+                            required=False)
+    card_city = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'شهر'}),
+                            required=True)
+    card_state = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'منطقه'}),
+                            required=False)
+    card_zipcode = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'کدپستی'}),
+                            required=True)
+    card_country = forms.CharField(label='', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'کشور'}),
+                            required=True)
--- payment/views.py
+++ payment/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from payment.forms import ShippingForm
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress
+from django.contrib import messages
 
+
+def billing_info(request):
+    if request.POST:
+        # Get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+
+        # Check to see if user is logged in
+        if request.user.is_authenticated:
+            # Get the billing form
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html',
+                          {'cart_products': cart_products, 'quantities': quantities, 'totals': totals,
+                           'shipping_info': request.POST, 'billing_form': billing_form})
+        else:
+            # Not logged in
+            # Get the billing form
+            billing_form = PaymentForm()
+            return render(request, 'payment/billing_info.html',
+                          {'cart_products': cart_products, 'quantities': quantities, 'totals': totals,
+                           'shipping_info': request.POST, 'billing_form': billing_form})
+
+
+        shipping_form = request.POST
+        return render(request, 'payment/billing_info.html', {'cart_products': cart_products, 'quantities': quantities, 'totals': totals, 'shipping_form': shipping_form})
+
+    else:
+        messages.success(request, 'دسترسی به صفحه مورد نظر لغو شد.')
+        return redirect('home')
 
 def checkout(request):
     # Get the cart
