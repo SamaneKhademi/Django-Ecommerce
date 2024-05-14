18e5ddf4f3877e770173f61734d1153ab685799b
Samane Khademi <s.khademi1992@gmail.com>
2024-05-14 23:08:24 +0330

    Automatically add shipping date

--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product, Order
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+import datetime
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -40,9 +42,23 @@ class Order(models.Model):
     amount_paid = models.DecimalField(max_digits=12, decimal_places=0)
     date_ordered = models.DateTimeField(auto_now_add=True)
     shipped = models.BooleanField(default=False)
+    date_shipped = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f'Order - {str(self.id)}'
+
+# Auto add shipping date
+@receiver(pre_save, sender=Order)
+def set_shipped_date_on_update(sender, instance, **kwargs):
+    if instance.pk:
+        now = datetime.datetime.now()
+        obj = sender._default_manager.get(pk=instance.pk)
+        if instance.shipped and not obj.shipped:
+            instance.date_shipped = now
+
+
+
+
 
 # Create order item model
 class OrderItem(models.Model):
