065bf030ba3e35ca13520aa80777514c024bb019
Samane Khademi <s.khademi1992@gmail.com>
2024-07-06 02:10:49 +0330

    Shipped buttons

--- payment/views.py
+++ payment/views.py
@@ -5,6 +5,7 @@ from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib.auth.models import User
 from django.contrib import messages
 from store.models import Product
+import datetime
 
 def orders(request, pk):
     if request.user.is_authenticated and request.user.is_superuser:
@@ -13,6 +14,22 @@ def orders(request, pk):
         # Get the order items
         items = OrderItem.objects.filter(order=pk)
 
+        if request.POST:
+            status = request.POST['shipping_status']
+            #Check if true or false
+            if status == 'true':
+                #Get the order
+                order = Order.objects.filter(id=pk)
+                # Update the status
+                now = datetime.datetime.now()
+                order.update(shipped=True, date_shipped=now)
+            else:
+                # Get the order
+                order = Order.objects.filter(id=pk)
+                # Update the status
+                order.update(shipped=False)
+            messages.success(request, 'وضعیت ارسال بروزرسانی شد')
+            return redirect('home')
 
         return render(request, 'payment/orders.html', {"order": order, "items": items})
 
@@ -24,6 +41,18 @@ def orders(request, pk):
 def not_shipped_dash(request):
     if request.user.is_authenticated and request.user.is_superuser:
         orders = Order.objects.filter(shipped=False)
+        if request.POST:
+            status = request.POST['shipping_status']
+            num = request.POST['num']
+            # Grab the order
+            order = Order.objects.filter(id=num)
+            # Grab date and time
+            now = datetime.datetime.now()
+            # Update order
+            order.update(shipped=True, date_shipped=now)
+
+            messages.success(request, 'وضعیت ارسال بروزرسانی شد')
+            return redirect('home')
         return render(request, 'payment/not_shipped_dash.html',{"orders":orders})
     else:
         messages.success(request, 'دسترسی شما لغو شد.')
@@ -31,6 +60,19 @@ def not_shipped_dash(request):
 def shipped_dash(request):
     if request.user.is_authenticated and request.user.is_superuser:
         orders = Order.objects.filter(shipped=True)
+        if request.POST:
+            status = request.POST['shipping_status']
+            num = request.POST['num']
+            # Grab the order
+            order = Order.objects.filter(id=num)
+
+            # Grab date and time
+            now = datetime.datetime.now()
+            # Update order
+            order.update(shipped=False)
+
+            messages.success(request, 'وضعیت ارسال بروزرسانی شد')
+            return redirect('home')
         return render(request, 'payment/shipped_dash.html', {"orders":orders})
     else:
         messages.success(request, 'دسترسی شما لغو شد.')
